8159286da3e3ea6ae31cff90c2f9dc76f4447c3f
Gavin <gaprice@lbl.gov>
2019-06-11 11:51:04 -0700

    Get tests to pass on a python3 only machine
    Also check in classpath & project files

--- test/cfg_to_runner.py
+++ test/cfg_to_runner.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 '''
 Created on Aug 30, 2013
 
 @author: gaprice@lbl.gov
 '''
-from configobj import ConfigObj
+import configparser
 import os
 import sys
 
@@ -26,21 +26,22 @@ if __name__ == '__main__':
     d, _ = os.path.split(os.path.abspath(__file__))
     fn = 'test.cfg'
     if len(sys.argv) < 2:
-        print 'Must provide the name of the jar file to test against'
+        print('Must provide the name of the jar file to test against')
     if len(sys.argv) > 2:
         fn = sys.argv[2]
     fn = os.path.join(d, fn)
     if not os.path.isfile(fn):
-        print 'No such config file ' + fn + '. Halting.'
+        print('No such config file ' + fn + '. Halting.')
         sys.exit(1)
-    print 'Using test config file ' + fn
+    print('Using test config file ' + fn)
     out = os.path.join(d, 'run_tests.sh')
-    cfg = ConfigObj(fn)
+    cfg = configparser.ConfigParser()
+    cfg.read(fn)
     try:
         testcfg = cfg[CFG_SECTION]
     except KeyError as ke:
-        print 'Test config file ' + fn + ' is missing section ' +\
-            CFG_SECTION + '. Halting.'
+        print('Test config file ' + fn + ' is missing section ' +\
+            CFG_SECTION + '. Halting.')
         sys.exit(1)
     with open(out, 'w') as run:
         run.write('# Generated file - do not check into git\n')
@@ -51,4 +52,4 @@ if __name__ == '__main__':
                 run.write(' -D' + o + '=' + testcfg[o])
         run.write(' -D' + JAR_OPT + '=' + sys.argv[1])
         run.write('\n')
-    os.chmod(out, 0755)
+    os.chmod(out, 0o755)
